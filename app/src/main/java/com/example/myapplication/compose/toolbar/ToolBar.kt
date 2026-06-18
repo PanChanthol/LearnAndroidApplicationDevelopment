5b62ad5bfe80a957635eec0a0aa97d6ba4c1eb35
PanChanthol <dannielgaming909@gmail.com>
2026-06-18 21:02:42 +0700

    Create toolbar and apply scroll behavior in top app bar

--- app/src/main/java/com/example/myapplication/compose/toolbar/ToolBar.kt
+++ app/src/main/java/com/example/myapplication/compose/toolbar/ToolBar.kt
@@ -1,41 +1,153 @@
 package com.example.myapplication.compose.toolbar
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScreenToolBar(){
+fun ScreenToolBar() {
+    val scrollBehavior =
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(state = rememberTopAppBarState())
     Scaffold(
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            CenterAlignedTopAppBar(
+            LargeTopAppBar(
                 title = {
                     Text("ToolBar")
-                }
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_arrow_back),
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    scrolledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                ),
+                scrollBehavior = scrollBehavior
             )
         },
         bottomBar = {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Surface(
+                    shape = RoundedCornerShape(28.dp),
+                    color = Color(0xFFF1EFF5),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp, vertical = 10.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = { }) {
+                            Icon(Icons.Filled.Check, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { }) {
+                            Icon(
+                                Icons.Filled.Edit,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                        IconButton(onClick = { }) {
+                            Icon(
+                                Icons.Filled.Mic,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                        IconButton(onClick = { }) {
+                            Icon(
+                                Icons.Filled.Image,
+                                contentDescription = "Localized description",
+                            )
+                        }
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.primaryContainer,
+                            shape = RoundedCornerShape(28.dp))
+                        .size(56.dp)
+                        .padding(10.dp),
+                    contentAlignment = Alignment.Center) {
+                    Icon(
+                        imageVector = Icons.Filled.Call,
+                        contentDescription = "End call",
+                        tint = Color(0xFF8A2E2E),
+                        modifier = Modifier.rotate(135f)
+                    )
+                }
+            }
         }
-    ) {padding->
-        Column(
-            modifier = Modifier.padding(padding)
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+//            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-
+            items(100){
+                Text(
+                    text = "Item $it",
+                    modifier = Modifier.padding(16.dp)
+                )
+            HorizontalDivider()
+            }
         }
     }
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = "id:pixel_9_pro")
 fun ScreenToolBarPreview() {
     MaterialTheme {
         MyApplicationTheme {
